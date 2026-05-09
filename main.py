f832e1e69afbb1c70a9c35ea4de5f166f77dda8e
suxql <20226410@s.ubaguio.edu>
2026-05-09 09:38:41 +0800

    write csv using dictionary

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import csv
 
+#-------------------------------------------------
 with open("output.csv", "w", newline="") as file:
+    '''
+        Write csv file using csv.writer
+    '''
     writer = csv.writer(file)
     writer.writerow(["id", "product", "price"])
     writer.writerow([1, "Laptop", 45000])
+#--------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------
+with open("users.csv", "w", newline="") as file:
+    '''
+        Write csv file using dictionaries
+    '''
+
+    fieldnames = ['id', 'name', 'email']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'id': 1, 'name': 'Anna', 'email': 'anna@gmail.com'})
+#--------------------------------------------------------------------------
